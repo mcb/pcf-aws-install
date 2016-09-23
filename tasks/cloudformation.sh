ecd683f4f3b6d1a020204587f73e4011de4da4b0
patrickcrocker <patrick.crocker@gmail.com>
2016-09-23 16:02:55 -0500

    updated

--- tasks/cloudformation.sh
+++ tasks/cloudformation.sh
@@ -6,4 +6,6 @@ export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 
+ls -al cloudformation
+
 aws iam list-server-certificates
