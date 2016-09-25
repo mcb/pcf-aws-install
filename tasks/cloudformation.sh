2574407bfc6b6795baa3531c4586f55865a8c3c9
patrickcrocker <patrick.crocker@gmail.com>
2016-09-25 10:10:10 -0500

    fixed parameter syntax

--- tasks/cloudformation.sh
+++ tasks/cloudformation.sh
@@ -4,10 +4,11 @@ set -e
 
 template=$(ls cloudformation/*cloudformation.json)
 
-params="ParameterKey=01NATKeyPair,ParameterValue=pcf"
-params="$params,ParameterKey=05RdsUsername,ParameterValue=boshuser"
-params="$params,ParameterKey=06RdsPassword,ParameterValue=boshpass"
-params="$params,ParameterKey=07SSLCertificateARN,ParameterValue=$AWS_SSL_CERTIFICATE_ARN"
+params=
+params="$params ParameterKey=01NATKeyPair,ParameterValue=pcf"
+params="$params ParameterKey=05RdsUsername,ParameterValue=boshuser"
+params="$params ParameterKey=06RdsPassword,ParameterValue=boshpass"
+params="$params ParameterKey=07SSLCertificateARN,ParameterValue=$AWS_SSL_CERTIFICATE_ARN"
 
 aws cloudformation create-stack \
     --stack-name pcf \
