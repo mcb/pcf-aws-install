1caedebf6fa525d6e2c9e5dea26df8d8ae0e318f
patrickcrocker <patrick.crocker@gmail.com>
2016-09-25 10:57:21 -0500

    wait till stack-create-complete

--- tasks/create-stack.sh
+++ tasks/create-stack.sh
@@ -4,13 +4,17 @@ set -e -x
 
 template=$(ls cloudformation/*cloudformation.json)
 
+stackname=pcf
+
 params="ParameterKey=01NATKeyPair,ParameterValue=pcf"
 params="$params ParameterKey=05RdsUsername,ParameterValue=boshuser"
 params="$params ParameterKey=06RdsPassword,ParameterValue=boshpass"
 params="$params ParameterKey=07SSLCertificateARN,ParameterValue=$AWS_SSL_CERTIFICATE_ARN"
 
 aws cloudformation create-stack \
-    --stack-name pcf \
+    --stack-name $stackname \
     --template-body file://$template \
     --capabilities CAPABILITY_IAM \
     --parameters $params
+
+aws cloudformation wait stack-create-complete --stack-name $stackname
