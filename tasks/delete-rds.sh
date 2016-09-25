aa66b98f30af734c24afda39492e60501f7e7ec7
patrickcrocker <patrick.crocker@gmail.com>
2016-09-25 16:25:06 -0500

    cleaned up script

--- tasks/delete-rds.sh
+++ tasks/delete-rds.sh
@@ -4,10 +4,12 @@ set -e -x
 
 stackname=$AWS_CLOUDFORMATION_STACK_NAME
 
-rdsAddress=$(aws cloudformation describe-stacks --stack-name $stackname | jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "PcfRdsAddress") | .OutputValue')
+stack=$(aws cloudformation describe-stacks --stack-name $stackname)
+rdsAddress=$(echo $stack | jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "PcfRdsAddress") | .OutputValue')
 
-instanceId=$(aws rds describe-db-instances | jq -r ".DBInstances[] | select(.Endpoint.Address == \"$rdsAddress\") | .DBInstanceIdentifier")
+dbInstances=$(aws rds describe-db-instances)
+dbInstanceId=$(echo $dbInstances | jq -r ".DBInstances[] | select(.Endpoint.Address == \"$rdsAddress\") | .DBInstanceIdentifier")
 
-aws rds delete-db-instance --skip-final-snapshot --db-instance-identifier $instanceId
+aws rds delete-db-instance --skip-final-snapshot --db-instance-identifier $dbInstanceId
 
-aws rds wait db-instance-deleted --db-instance-identifier $instanceId
+aws rds wait db-instance-deleted --db-instance-identifier $dbInstanceId
