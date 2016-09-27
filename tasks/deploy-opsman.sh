e7c00bd26ea84eb6a338fcb2d2134419ea6965dd
patrickcrocker <patrick.crocker@gmail.com>
2016-09-26 21:53:59 -0500

    create elasticIp and route53 recordset

--- tasks/deploy-opsman.sh
+++ tasks/deploy-opsman.sh
@@ -4,7 +4,9 @@ set -e -x
 
 stackname=$AWS_CLOUDFORMATION_STACK_NAME
 opsmanAmi=$OPS_MANAGER_AMI
+opsmanDomain=$OPS_MANAGER_DOMAIN
 keyName=$AWS_KEY_NAME
+hostedZoneId=$AWS_HOSTED_ZONE_ID
 
 stack=$(aws cloudformation describe-stacks --stack-name $stackname)
 
@@ -26,3 +28,37 @@ ec2InstanceId=$(echo $ec2Instance | jq -r ".Instances[0].InstanceId")
 aws ec2 create-tags --resources $ec2InstanceId --tags "Key=Name,Value=Ops Manager"
 
 aws ec2 wait instance-running --instance-ids $ec2InstanceId
+
+allocateAddress=$(aws ec2 allocate-address --domain vpc)
+
+publicId=$(echo allocateAddress | jq -r '.PublicIp')
+allocationId=$(echo allocateAddress | jq -r '.AllocationId')
+
+aws ec2 associate-address --instance-id $ec2InstanceId --public-ip $publicId --allocation-id $allocationId
+
+cat <<EOF >change-resource-record-sets.json
+{
+  "Comment": "create record set for opsmanager",
+  "Changes": [
+    {
+      "Action": "CREATE",
+      "ResourceRecordSet": {
+        "Name": "$opsmanDomain",
+        "Type": "A",
+        "TTL": 300,
+        "ResourceRecords": [
+          {
+            "Value": "$publicId"
+          }
+        ]
+      }
+    }
+  ]
+}
+EOF
+
+createRecordSet=$(aws route53 change-resource-record-sets --hosted-zone-id $hostedZoneId --change-batch file://change-resource-record-sets.json)
+
+changeId=$(echo allocateAddress | jq -r '.ChangeInfo.Id')
+
+aws route53 wait resource-record-sets-changed --id $changeId
