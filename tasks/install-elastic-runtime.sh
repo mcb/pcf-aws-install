86d87832debc0c9c751f75b6777790dc00b4d2f2
patrickcrocker <patrick.crocker@gmail.com>
2016-09-30 15:35:20 -0500

    added install-elastic-runtime

---
+++ tasks/install-elastic-runtime.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e -x
+
+opsmanDomain=$OPS_MANAGER_DOMAIN
+adminUser=$OPS_MANAGER_ADMIN_USER
+adminPass=$OPS_MANAGER_ADMIN_PASS
+
+# Login to UAA
+uaac target https://$opsmanDomain/uaa --skip-ssl-validation
+
+uaac token owner get opsman $adminUser -p $adminPass -s ''
+
+UAA_ACCESS_TOKEN=$(uaac context admin | grep access_token | sed -e 's/^\s*access_token:\s//')
+
+# Upload elastic-runtime
+file=$(ls elastic-runtime/cf-*.pivotal)
+
+curl "https://example.com/api/v0/available_products" \
+    -X POST \
+    -H "Authorization: Bearer $UAA_ACCESS_TOKEN" \
+    -F "product[file]=@$file"
