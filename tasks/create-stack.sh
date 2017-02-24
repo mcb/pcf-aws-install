ba8b23b356ff410186f75ec77cd4f65052124edc
patrickcrocker <patrick.crocker@gmail.com>
2017-02-24 15:14:22 -0600

    added support to not provision rds

--- tasks/create-stack.sh
+++ tasks/create-stack.sh
@@ -2,21 +2,18 @@
 
 set -e -x
 
-stackName=$AWS_CLOUDFORMATION_STACK_NAME
-sslcertarn=$AWS_SSL_CERTIFICATE_ARN
-keyName=$AWS_KEY_NAME
+params="ParameterKey=01NATKeyPair,ParameterValue=$AWS_KEY_NAME"
+params="$params ParameterKey=04RdsDBName,ParameterValue=$RDS_DBNAME"
+params="$params ParameterKey=05RdsUsername,ParameterValue=$RDS_USERNAME"
+params="$params ParameterKey=06RdsPassword,ParameterValue=$RDS_PASSWORD"
+params="$params ParameterKey=07SSLCertificateARN,ParameterValue=$AWS_SSL_CERTIFICATE_ARN"
 
 template=$(ls cloudformation/*cloudformation.json)
 
-params="ParameterKey=01NATKeyPair,ParameterValue=$keyName"
-params="$params ParameterKey=05RdsUsername,ParameterValue=boshuser"
-params="$params ParameterKey=06RdsPassword,ParameterValue=boshpass"
-params="$params ParameterKey=07SSLCertificateARN,ParameterValue=$sslcertarn"
-
 aws cloudformation create-stack \
-    --stack-name $stackName \
+    --stack-name $AWS_CLOUDFORMATION_STACK_NAME \
     --template-body file://$template \
     --capabilities CAPABILITY_IAM \
     --parameters $params
 
-aws cloudformation wait stack-create-complete --stack-name $stackName
+aws cloudformation wait stack-create-complete --stack-name $AWS_CLOUDFORMATION_STACK_NAME
