b74a176b6207765997402ea78e7293a836bcc924
patrickcrocker <patrick.crocker@gmail.com>
2017-02-24 20:13:06 -0600

    remove rds output if no rds specified

--- tasks/create-stack.sh
+++ tasks/create-stack.sh
@@ -8,14 +8,26 @@ params="$params ParameterKey=05RdsUsername,ParameterValue=$RDS_USERNAME"
 params="$params ParameterKey=06RdsPassword,ParameterValue=$RDS_PASSWORD"
 params="$params ParameterKey=07SSLCertificateARN,ParameterValue=$AWS_SSL_CERTIFICATE_ARN"
 
-template=$(ls cloudformation/*cloudformation.json)
+cloudformation=$(ls cloudformation/*cloudformation.json)
+template=template.json
 
-#
-cat $template | jq -r '.Parameters."04RdsDBName".MinLength = 0 | .Parameters."06RdsPassword".MinLength = 0' > template.json
+if [ -z $RDS_DBNAME ] && [ -z $RDS_USERNAME ] && [ -z $RDS_PASSWORD ]; then
+  cat $cloudformation |
+    jq -r ' .Parameters."04RdsDBName".MinLength = 0
+          | .Parameters."06RdsPassword".MinLength = 0
+          | del(.Outputs.PcfRdsDBName)
+          | del(.Outputs.PcfRdsPassword)
+          | del(.Outputs.PcfRdsUsername)
+          | del(.Outputs.PcfRdsPort)
+          | del(.Outputs.PcfRdsAddress)' \
+          > $template
+else
+  $template=$cloudformation
+fi
 
 aws cloudformation create-stack \
     --stack-name $AWS_CLOUDFORMATION_STACK_NAME \
-    --template-body file://template.json \
+    --template-body file://$template \
     --capabilities CAPABILITY_IAM \
     --parameters $params
 
